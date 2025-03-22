43226b2eaef07454ea03022e5892352ec1fc3dde
tugceer <htugceerr@hotmail.com>
2025-03-22 22:26:37 +0300

    Insertion Sort kodu eklendi.

---
+++ InsertionSort/insertionsort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class InsertionSortProgram
+{
+    static void InsertionSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 1; i < n; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        Console.WriteLine(string.Join(" ", arr));
+    }
+
+    static void Main()
+    {
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+
+        Console.WriteLine("Orijinal dizi:");
+        PrintArray(arr);
+
+        Console.WriteLine("\nInsertion Sort Sonucu:");
+        InsertionSort(arr);
+        PrintArray(arr);
+
+        Console.WriteLine("\nÇıkmak için bir tuşa basın...");
+        Console.ReadKey();
+    }
+}
