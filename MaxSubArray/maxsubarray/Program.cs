60c5379407b65b5184d3e35edc2efd83c5e234c0
tugceer <htugceerr@hotmail.com>
2025-03-22 22:41:25 +0300

    yeni kod eklendi.

---
+++ MaxSubArray/maxsubarray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int maxSum = MaxSubarray(nums);
+        Console.WriteLine("Maksimum Alt Dizi Toplamı: " + maxSum);
+    }
+
+    static int MaxSubarray(int[] nums)
+    {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            currentSum = Math.Max(nums[i], currentSum + nums[i]);
+            maxSum = Math.Max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
