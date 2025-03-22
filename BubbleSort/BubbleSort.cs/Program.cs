a5a24a01f8c0fd4eebecb85ae2f663c1d06f9623
tugceer <htugceerr@hotmail.com>
2025-03-22 22:17:00 +0300

    Bubble Sort kodu eklendi

---
+++ BubbleSort/BubbleSort.cs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class BubbleSortProgram
+{
+    static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Swap işlemi
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        Console.WriteLine(string.Join(" ", arr));
+    }
+
+    static void Main()
+    {
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+
+        Console.WriteLine("Orijinal dizi:");
+        PrintArray(arr);
+
+        Console.WriteLine("\nBubble Sort Sonucu:");
+        BubbleSort(arr);
+        PrintArray(arr);
+
+        Console.WriteLine("\nÇıkmak için bir tuşa basın...");
+        Console.ReadKey();
+    }
+}
